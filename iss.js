466228c405989d44cd0c6b7bdadd40da09efbfa4
Aviva Wolpert <wolpertaviva@gmail.com>
2015-09-16 18:47:21 +0000

    iss-augmented. still working on syntax

--- iss.js
+++ iss.js
@@ -1,5 +1,7 @@
 // 1. output the latitude and longitude of the International Space Station.
 
+
+
 var request = require('request');
 
 request('http://api.open-notify.org/iss-now.json', function(error, response, body) {
@@ -13,3 +15,64 @@ request('http://api.open-notify.org/iss-now.json', function(error, response, bod
     }
     
 });
+
+Number.prototype.toRadians = function() {
+    return this * Math.PI / 180;
+   
+}
+
+ var prompt = require('prompt');
+
+  prompt.start();
+
+  prompt.get(['location'], function (err, result) {
+    if (err) { return onErr(err); }
+    console.log('Command-line input received:');
+    console.log('  Location: ' + result.location);
+  });
+
+  function onErr(err) {
+    console.log("Error. Try again.");
+    return 1;
+  }
+  
+  console.log('  --  user location: ' + result.location);
+        issInfo(result.location);
+    });
+}
+
+function findDistance(usrLoc, issLat, issLon){
+    request('https://maps.googleapis.com/maps/api/geocode/json?address=' + usrLoc +"", function(error, response, body) {
+        if(!error){
+            var userLocationInfo = JSON.parse(body);
+            var userLat = userLocationInfo.results[0].geometry.location.lat;
+            var userLon = userLocationInfo.results[0].geometry.location.lng;
+            
+        }
+        else {
+            console.log("Error. Try again.");
+        }
+       
+        
+    });
+   
+
+   function distanceISS(lat1,lon1,lat2,lon2){
+   
+    var R = 6371000; // metres
+var φ1 = lat1.toRadians();
+var φ2 = lat2.toRadians();
+var Δφ = (lat2-lat1).toRadians();
+var Δλ = (lon2-lon1).toRadians();
+
+var a = Math.sin(Δφ/2) * Math.sin(Δφ/2) +
+        Math.cos(φ1) * Math.cos(φ2) *
+        Math.sin(Δλ/2) * Math.sin(Δλ/2);
+var c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1-a));
+
+var d = R * c;
+   }
+
+    console.log("The distance between you and the ISS is: " + Math.round( distanceISS(userLat, userLon, issLat, issLon)/1000 ) + " kilometers")
+    
+   })
